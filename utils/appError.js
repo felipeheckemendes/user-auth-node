bfc310183f7a847637a609b560e44480de3a53e4
felipeheckemendes <157503990+felipeheckemendes@users.noreply.github.com>
2024-12-20 06:39:38 -0300

    feat: add AppError class
    The AppError class extends the Error class by adding HTTP statusCode and status

---
+++ utils/appError.js
@@ -0,0 +1,8 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = statusCode.toString().startsWith('4') ? 'fail' : 'error';
+    Error.captureStackTrace(this, this.constructor); // Exclude constructor function from the error call stack trace
+  }
+}
