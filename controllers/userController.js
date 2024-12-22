97148d9a0b3030ddff9a1697505a29c98375dc3e
felipeheckemendes <157503990+felipeheckemendes@users.noreply.github.com>
2024-12-21 21:59:45 -0300

    feat: add function to create and send JWT token on JSON response

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,21 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
+
+const createAndSendToken = (statusCode, user, res) => {
+  const payload = {
+    id: user._id,
+    iat: Math.floor(Date.now() / 1000 - 10),
+    exp: Math.floor(Date.now() / 1000 + 60 * 24 * process.env.JWT_EXPIRES_DAYS),
+  };
+  const token = jwt.sign(payload, process.env.JWTSECRET);
+  res.status(statusCode).json({
+    status: 'success',
+    token: token,
+    data: user,
+  });
+};
 
 exports.signup = async (req, res, next) => {
   const user = await User.create(req.body);
-  res.status(201).json({
-    status: 'success',
-    user: user,
-  });
+  createAndSendToken(201, user, res);
 };
