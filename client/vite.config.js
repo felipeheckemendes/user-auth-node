617997f90a166b4622634d7c50703c436cfd186b
felipeheckemendes <157503990+felipeheckemendes@users.noreply.github.com>
2025-01-05 21:40:43 -0300

    chore: install shadcn UI lib

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // Setting the alias "@" to refer to the "src" directory
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
