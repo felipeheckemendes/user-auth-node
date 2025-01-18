cfa1557920830875308c470861e7962c2d39dd28
felipeheckemendes <157503990+felipeheckemendes@users.noreply.github.com>
2025-01-18 13:59:35 -0300

    feat(front-end): create initial route structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from 'react';
+// Dependencies
+import { createRoutesFromElements, createBrowserRouter, Route } from 'react-router';
+// CSS
 import './App.css';
+// Components
 
 function App() {
-  return (
-    <>
-      <h1>Initialized front-end</h1>
-    </>
+  return createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="" element={<Layout />}>
+        <Route path="" element={<Home />} />
+        <Route path="account">
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="profile" element={<AccountLayout />}>
+            <Route path="overview" element={<AccountOverview />} />
+            <Route path="info" element={<AccountInfo />} />
+          </Route>
+        </Route>
+        <Route path="users" element={<UsersLayout />}>
+          <Route index element={<UsersList />} />
+          <Route path=":id" element={<UpdateUser />} />
+          <Route path=":id/update" element={<UpdateUser />} />
+          <Route path="createuser" element={<CreateUser />} />
+        </Route>
+        <Route path="docs" element={<DocsLayout />}>
+          <h1>Docs Subroutes here</h1>
+        </Route>
+      </Route>,
+    ),
   );
 }
 
